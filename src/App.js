0e4de6e988a2a760c015079406b74f66d4b5441f
KSentak <k.sentak@outlook.com>
2020-03-12 15:18:27 -0400

    Added colorHelper

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Palette from './components/Palette';
 import seedColors from './seedColors';
+import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <div className='App'>
-      <Palette {...seedColors[2]}/>
-    </div>
-  );
+class App extends Component {
+  render() {
+    console.log(generatePalette(seedColors[4]));
+
+    return (
+      <div className='App'>
+        <Palette {...seedColors[2]} />
+      </div>
+    );
+  }
 }
 
 export default App;
