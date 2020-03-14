e87921294779b48f12b969f61fd07918f657b3dd
KSentak <k.sentak@outlook.com>
2020-03-14 15:43:38 -0400

    Added routing to application

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Palette from './components/Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
@@ -8,9 +9,18 @@ class App extends Component {
     console.log(generatePalette(seedColors[4]));
 
     return (
-      <div className='App'>
-        <Palette palette={generatePalette(seedColors[4])} />
-      </div>
+      <Switch>
+        <Route exact path='/' render={() => <h1>PALETTE LIST GOES HERE</h1>} />
+        <Route
+          exact
+          path='/palette/:id'
+          render={() => <h1>Individual Palette</h1>}
+        />
+      </Switch>
+
+      // <div className='App'>
+      //   <Palette palette={generatePalette(seedColors[4])} />
+      // </div>
     );
   }
 }
