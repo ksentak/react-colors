6fc698483853d6d674854b07b832607a2478c09a
KSentak <k.sentak@outlook.com>
2020-03-14 14:54:00 -0400

    Added select section to navbar component

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import Slider from 'rc-slider';
-
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
 import 'rc-slider/assets/index.css';
 import './css/Navbar.css';
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: 'hex' };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({ format: event.target.value });
+    this.props.handleChange(event.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className='Navbar'>
         <div className='logo'>
@@ -23,6 +35,13 @@ class Navbar extends Component {
               onAfterChange={changeLevel}
             />
           </div>
+        </div>
+        <div className='select-container'>
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value='hex'>HEX - #ffffff </MenuItem>
+            <MenuItem value='rgb'>RGB - rbg(255,255,255) </MenuItem>
+            <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0) </MenuItem>
+          </Select>
         </div>
       </header>
     );
