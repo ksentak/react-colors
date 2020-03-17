89122f33656520a5151fd9bb24f8926ee5139169
KSentak <k.sentak@outlook.com>
2020-03-17 12:56:27 -0400

    Start styling minipalette

---
+++ src/components/MiniPalette.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { withStyles } from '@material-ui/styles';
+
+const styles = {
+  root: {
+    backgroundColor: 'white',
+    border: '1px solid black',
+    borderRadius: '5px',
+    padding: '0.5rem',
+    position: 'relative',
+    overflow: 'hidden',
+    '&:hover': {
+      cursor: 'pointer',
+    },
+  },
+  colors: {
+    backgroundColor: 'grey',
+  },
+  title: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    margin: '0',
+    color: 'black',
+    paddingTop: '0.5rem',
+    fontSize: '1rem',
+    position: 'relative',
+  },
+};
+
+function MiniPalette(props) {
+  const { classes, paletteName } = props;
+  return (
+    <div className={classes.root}>
+      <div className={classes.colors}></div>
+      <h5 className={classes.title}>{paletteName}</h5>
+    </div>
+  );
+}
+
+export default withStyles(styles)(MiniPalette);
--- src/components/PaletteList.js
+++ src/components/PaletteList.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 class PaletteList extends Component {
   render() {
     const { palettes } = this.props;
     return (
       <div>
+        <MiniPalette />
         <h1>React-colors</h1>
         {palettes.map(palette => (
-          <p>
-            <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-          </p>
+          <MiniPalette {...palette} />
         ))}
       </div>
     );
