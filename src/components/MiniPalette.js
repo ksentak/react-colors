3e31bf2e82b454f017dcb9645be5efe99f951c44
KSentak <k.sentak@outlook.com>
2020-03-18 12:37:32 -0400

    Updated minipalette and navbar components

--- src/components/MiniPalette.js
+++ src/components/MiniPalette.js
@@ -1,67 +1,46 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { withStyles } from '@material-ui/styles';
+import styles from './styles/MiniPaletteStyles';
+import DeleteIcon from '@material-ui/icons/Delete';
 
-const styles = {
-  root: {
-    backgroundColor: 'white',
-    border: '1px solid black',
-    borderRadius: '5px',
-    padding: '0.5rem',
-    position: 'relative',
-    overflow: 'hidden',
-    '&:hover': {
-      cursor: 'pointer'
-    }
-  },
-  colors: {
-    backgroundColor: '#dae1e4',
-    height: '100px',
-    width: '100%',
-    borderRadius: '5px',
-    overflow: 'hidden'
-  },
-  title: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-    margin: '0',
-    color: 'black',
-    paddingTop: '0.5rem',
-    fontSize: '1rem',
-    position: 'relative'
-  },
-  emoji: {
-    marginLeft: '0.5rem',
-    fontSize: '1.5rem'
-  },
-  miniColor: {
-    height: '25%',
-    width: '20%',
-    display: 'inline-block',
-    margin: '0 auto',
-    position: 'relative',
-    marginBottom: '-4px'
+class MiniPalette extends PureComponent {
+  constructor(props) {
+    super(props);
+    this.deletePalette = this.deletePalette.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
-};
+  deletePalette(e) {
+    e.stopPropagation();
+    this.props.openDialog(this.props.id);
+  }
+  handleClick() {
+    this.props.goToPalette(this.props.id);
+  }
+  render() {
+    const { classes, paletteName, emoji, colors } = this.props;
 
-function MiniPalette(props) {
-  const { classes, paletteName, emoji, colors } = props;
-  const miniColorBoxes = colors.map(color => (
-    <div
-      className={classes.miniColor}
-      style={{ backgroundColor: color.color }}
-      key={color.name}
-    />
-  ));
+    const miniColorBoxes = colors.map(color => (
+      <div
+        className={classes.miniColor}
+        style={{ backgroundColor: color.color }}
+        key={color.name}
+      />
+    ));
+    return (
+      <div className={classes.root} onClick={this.handleClick}>
+        <DeleteIcon
+          className={classes.deleteIcon}
+          style={{ transition: 'all 0.3s ease-in-out' }}
+          onClick={this.deletePalette}
+        />
 
-  return (
-    <div className={classes.root} onClick={props.handleClick}>
-      <div className={classes.colors}>{miniColorBoxes}</div>
-      <h5 className={classes.title}>
-        {paletteName} <span className={classes.emoji}>{emoji}</span>
-      </h5>
-    </div>
-  );
+        <div className={classes.colors}>{miniColorBoxes}</div>
+        <h5 className={classes.title}>
+          {paletteName} <span className={classes.emoji}>{emoji}</span>
+        </h5>
+      </div>
+    );
+  }
 }
 
 export default withStyles(styles)(MiniPalette);
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-
+import { withStyles } from '@material-ui/styles';
 import Select from '@material-ui/core/Select';
-import Snackbar from '@material-ui/core/Snackbar';
 import MenuItem from '@material-ui/core/MenuItem';
-import CloseIcon from '@material-ui/icons/Close';
+import Snackbar from '@material-ui/core/Snackbar';
 import IconButton from '@material-ui/core/IconButton';
-
+import CloseIcon from '@material-ui/icons/Close';
 import Slider from 'rc-slider';
-
 import 'rc-slider/assets/index.css';
-import './css/Navbar.css';
+import styles from './styles/NavbarStyles';
 
 class Navbar extends Component {
   constructor(props) {
@@ -19,28 +17,25 @@ class Navbar extends Component {
     this.handleFormatChange = this.handleFormatChange.bind(this);
     this.closeSnackbar = this.closeSnackbar.bind(this);
   }
-
-  handleFormatChange(event) {
-    this.setState({ format: event.target.value, open: true });
-    this.props.handleChange(event.target.value);
+  handleFormatChange(e) {
+    this.setState({ format: e.target.value, open: true });
+    this.props.handleChange(e.target.value);
   }
-
   closeSnackbar() {
     this.setState({ open: false });
   }
-
   render() {
-    const { level, changeLevel, showingAllColors } = this.props;
+    const { level, changeLevel, showingAllColors, classes } = this.props;
     const { format } = this.state;
     return (
-      <header className='Navbar'>
-        <div className='logo'>
-          <Link to='/'>React-Colors</Link>
+      <header className={classes.Navbar}>
+        <div className={classes.logo}>
+          <Link to='/'>reactcolorpicker</Link>
         </div>
         {showingAllColors && (
-          <div className='slider-container'>
+          <div>
             <span>Level: {level}</span>
-            <div className='slider'>
+            <div className={classes.slider}>
               <Slider
                 defaultValue={level}
                 min={100}
@@ -51,22 +46,25 @@ class Navbar extends Component {
             </div>
           </div>
         )}
-        <div className='select-container'>
+        <div className={classes.selectContainer}>
           <Select value={format} onChange={this.handleFormatChange}>
-            <MenuItem value='hex'>HEX - #ffffff </MenuItem>
-            <MenuItem value='rgb'>RGB - rbg(255,255,255) </MenuItem>
-            <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0) </MenuItem>
+            <MenuItem value='hex'>HEX - #ffffff</MenuItem>
+            <MenuItem value='rgb'>RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value='rgba'>RGBA - rgba(255,255,255, 1.0)</MenuItem>
           </Select>
         </div>
         <Snackbar
           anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
           open={this.state.open}
           autoHideDuration={3000}
-          message={<span id='message-id'>Format Changed.</span>}
+          message={
+            <span id='message-id'>
+              Format Changed To {format.toUpperCase()}
+            </span>
+          }
           ContentProps={{
             'aria-describedby': 'message-id'
           }}
-          // ON CLOSE ALLOWS USER TO CLICK ANYWHERE ON PAGE TO CLOSE SNACKBAR
           onClose={this.closeSnackbar}
           action={[
             <IconButton
@@ -83,5 +81,4 @@ class Navbar extends Component {
     );
   }
 }
-
-export default Navbar;
+export default withStyles(styles)(Navbar);
