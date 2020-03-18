20489f14d76a339fa3db4ac2858009aad9fc0243
KSentak <k.sentak@outlook.com>
2020-03-18 12:32:02 -0400

    Updated colorbox component

--- src/components/ColorBox.js
+++ src/components/ColorBox.js
@@ -1,21 +1,9 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { CopyToClipboard } from 'react-copy-to-clipboard';
-import chroma from 'chroma-js';
+import classNames from 'classnames';
 import { withStyles } from '@material-ui/styles';
-
-import './css/ColorBox.css';
-
-const styles = {
-  copyText: {
-    color: props =>
-      chroma(props.background).luminance() >= 0.7 ? 'black' : 'white'
-  },
-  colorName: {
-    color: props =>
-      chroma(props.background).luminance() <= 0.08 ? 'white' : 'black'
-  }
-};
+import styles from '../styles/ColorBoxStyles';
 
 class ColorBox extends Component {
   constructor(props) {
@@ -31,35 +19,42 @@ class ColorBox extends Component {
   }
 
   render() {
-    const { name, background, moreURL, showLink, classes } = this.props;
-    const { copied } = this.state;
-    const isDarkColor = chroma(background).luminance() <= 0.08;
-    const isLightColor = chroma(background).luminance() >= 0.7;
+    const {
+      name,
+      background,
+      moreUrl,
+      showingFullPalette,
+      classes
+    } = this.props;
 
+    const { copied } = this.state;
     return (
       <CopyToClipboard text={background} onCopy={this.changeCopyState}>
-        <div style={{ background }} className='ColorBox'>
+        <div style={{ background }} className={classes.ColorBox}>
           <div
             style={{ background }}
-            className={`copy-overlay ${copied && 'show'}`}
-          ></div>
-          <div className={`copy-msg ${copied && 'show'}`}>
+            className={classNames(classes.copyOverlay, {
+              [classes.showOverlay]: copied
+            })}
+          />
+
+          <div
+            className={classNames(classes.copyMessage, {
+              [classes.showMessage]: copied
+            })}
+          >
             <h1>copied!</h1>
-            <p className={classes.copyText}>{this.props.background}</p>
+            <p className={classes.copyText}>{background}</p>
           </div>
-          <div className='copy-container'>
-            <div className='box-content'>
+          <div>
+            <div className={classes.boxContent}>
               <span className={classes.colorName}>{name}</span>
             </div>
-            <button className={`copy-button ${isLightColor && 'dark-text'}`}>
-              Copy
-            </button>
+            <button className={classes.copyButton}>Copy</button>
           </div>
-          {showLink && (
-            <Link to={moreURL} onClick={event => event.stopPropagation()}>
-              <span className={`see-more ${isLightColor && 'dark-text'}`}>
-                MORE
-              </span>
+          {showingFullPalette && (
+            <Link to={moreUrl} onClick={e => e.stopPropagation()}>
+              <span className={classes.seeMore}>MORE</span>
             </Link>
           )}
         </div>
